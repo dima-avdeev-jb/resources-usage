0ebebde000f455d6b7dbfb21f04e2c3d65eafde6
dima.avdeev <dima.avdeev@jetbrains.com>
2022-11-29 17:04:17 +0400

    simplify

---
+++ README.md
@@ -0,0 +1,8 @@
+
+### Run Desktop
+`./gradlew :desktopApp:run`
+
+### Run MacOS
+ - on Intel CPU: `./gradlew :shared:runDebugExecutableMacosX64`
+ - on Apple M1: `./gradlew :shared:runDebugExecutableMacosArm64`
+
---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("multiplatform") apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        mavenLocal()
+    }
+}
+
